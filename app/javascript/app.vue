c0dc6c08ee4a668b12a60a2f79cb2ac9eb514275
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-26 15:56:47 +0000

    [Add] HelloVueアプリケーションを実装

---
+++ app/javascript/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>
+    {{ message }}
+  </p>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }
+</script>
+
+<style>
+  p {
+    font-size: 2em;
+    text-align: left;
+  }
+</style>
