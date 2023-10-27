ee7124bd8f64363e02a80e12049b2722436ddf48
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-27 04:04:28 +0000

    [Add] 読み取った値を表示するVueコンポーネントを実装しマウント

--- app/javascript/packs/homes/index.js
+++ app/javascript/packs/homes/index.js
@@ -1,5 +1,9 @@
+import { createApp } from 'vue';
+import Index from '../../components/homes/Index.vue';
+
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('homesIndex');
   const initialData = JSON.parse(node.getAttribute('data'));
-  console.log(initialData);
+  const app = createApp(Index, { initialData: initialData });
+  app.mount('#homesIndex');
 });
