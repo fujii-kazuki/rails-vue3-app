60ccbce447672970bdd9f6aff11d90deae239ed0
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-10-27 10:32:01 +0000

    [Add] リンクボタンを追加

--- app/javascript/components/homes/Contents.vue
+++ app/javascript/components/homes/Contents.vue
@@ -16,6 +16,9 @@
         </tr>
       </tbody>
     </table>
+    <div v-for="inner_link in contents.inner_links" v-bind:key="inner_link.label">
+      <button v-on:click="changeLocation(inner_link.url)" class="btn-push">{{ inner_link.label }}</button>
+    </div>
   </div>
 </template>
 
@@ -26,17 +29,39 @@ export default {
       type: Object,
       default: () => {}
     }
+  },
+  methods: {
+    changeLocation(url) {
+      window.location.href = url;
+    }
   }
 };
 </script>
 
 <style scoped>
 .contents-table {
-border: 1px solid gray;
-margin: 10px;
+  border: 1px solid gray;
+  margin: 10px;
 }
 .contents-table th,
 .contents-table td {
-border: 1px solid gray;
+  border: 1px solid gray;
+}
+.btn-push {
+  margin: 10px;
+  max-width: 180px;
+  text-align: left;
+  background-color: rgb(24, 174, 238);
+  font-size: 14px;
+  color: #fff;
+  text-decoration: none;
+  font-weight: bold;
+  padding: 10px 24px;
+  border-radius: 4px;
+  border-bottom: 4px solid rgb(24, 174, 238);
+}
+.btn-push:active {
+  transform: translateY(4px);
+  border-bottom: none;
 }
 </style>
